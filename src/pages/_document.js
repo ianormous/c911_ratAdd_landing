5043629383fbede0b7c7607b8e934e635c81ee29
ianormous <igentleswork@gmail.com>
2023-02-11 12:58:54 -0500

    initial commit

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,10 +3,14 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+
+      </Head>
       <body>
         <Main />
         <NextScript />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.4/gsap.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.4/ScrollTrigger.min.js"></script>
       </body>
     </Html>
   )
