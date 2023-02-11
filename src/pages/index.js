c95386dbd7ef0fda044a00c9352367f666ce6b61
ianormous <igentleswork@gmail.com>
2023-02-11 13:15:02 -0500

    minor changes so desktop looks paletable

--- src/pages/index.js
+++ src/pages/index.js
@@ -131,37 +131,39 @@ export default function Home() {
         </div>
 
         {/* about */}
-        <div className={`${styles.aboutContainer}`}>
-          <div className={styles.about_header}>
-            <span className={styles.about_header_emphasis}>FAST </span>
-            &gt;&gt;&gt; <br></br>
-            100% Effective Rat &<br></br>
-            Mouse Solutions
-          </div>
+        <div className={`d-flex justify-content-center ${styles.aboutWrapper}`}>
 
-          <div className={`d-flex flex-column justify-content-center align-items-center ${styles.about_subHeader}`}>
-            <div className={styles.about_subHeader1}>
-              Residential | Commercial | Multi Unit | Institutional
+
+          <div className={`${styles.aboutContainer}`}>
+            <div className={styles.about_header}>
+              <span className={styles.about_header_emphasis}>FAST </span>
+              &gt;&gt;&gt; <br></br>
+              100% Effective Rat &<br></br>
+              Mouse Solutions
             </div>
-            <div className={styles.about_subHeader2}>
-              Rodent (Rat & Mouse) Solutions
+
+            <div className={`d-flex flex-column justify-content-center align-items-center ${styles.about_subHeader}`}>
+              <div className={styles.about_subHeader1}>
+                Residential | Commercial | Multi Unit | Institutional
+              </div>
+              <div className={styles.about_subHeader2}>
+                Rodent (Rat & Mouse) Solutions
+              </div>
+            </div>
+
+            <div className={styles.about_ctaWrapper}>
+              <a className={styles.about_cta}>Don't Wait Call 407-719-6221</a>
+            </div>
+
+            <hr className={styles.about_hr} />
+
+            <div className={styles.about_bio}>
+              <div className={styles.happyFamilyWrapper}>
+                <Image src={happyFamily} className={styles.happyFamily}></Image>
+              </div>
+              Here at Orlando Critter 911 we understand! Nothing is more important to you than the health & safety of your family, home or business... Fact is that providing you with rat a& mouse free peace of mind is what we do best.
             </div>
           </div>
-
-          <div className={styles.about_ctaWrapper}>
-            <a className={styles.about_cta}>Don't Wait Call 407-719-6221</a>
-          </div>
-
-          <hr className={styles.about_hr} />
-
-          <div className={styles.about_bio}>
-            <div className={styles.happyFamilyWrapper}>
-              <Image src={happyFamily} className={styles.happyFamily}></Image>
-            </div>
-            Here at Orlando Critter 911 we understand! Nothing is more important to you than the health & safety of your family, home or business... Fact is that providing you with rat a& mouse free peace of mind is what we do best.
-          </div>
-
-
         </div>
 
         {/* IM BETTER */}
