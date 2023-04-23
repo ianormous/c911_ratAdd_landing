a882aab308fbb821a4d57241fcb53af95fb2c409
ianormous <igentleswork@gmail.com>
2023-04-23 13:21:58 -0400

    testing the pushing to main branch

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -29,7 +29,7 @@ export default function Landing({
                     <div className={`d-flex flex-column align-items-center ${styles.videoContainer}`}>
                         {/* header */}
                         <div className={`d-flex align-items-center justify-content-center ${styles.videoTextWrapper}`}>
-                            <div className={`${styles.videoText}`}>You Want Them Gone Now</div>
+                            <div className={`${styles.videoText}`}>You Want Them Gone Now OH MY GAWD</div>
                         </div>
 
                         {/* video player */}
