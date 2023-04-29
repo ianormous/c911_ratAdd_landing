23671d5c902a8bb6e4fff68b3f8d669428aa9c3e
ianormous <igentleswork@gmail.com>
2023-04-29 13:24:16 -0400

    spell check

--- src/pages/index.js
+++ src/pages/index.js
@@ -65,31 +65,31 @@ export default function Home() {
 const ratFacts = [
     {
         header: 'Disease Risk',
-        body: "Rats and mice make millions of people sick every year. Their dropping can spread diseases to humans, many of which can be fatal. Their feces dust, urine crystals, hair and dander. Rat skin cells cause asthma, allergies, inflammation and imminue deficiencies.",
+        body: "Rats and mice make millions of people sick every year. Their dropping can spread diseases to humans, many of which can be fatal. Their feces dust, urine crystals, hair and dander. Rat skin cells cause asthma, allergies, inflammation and immune deficiencies.",
         img: diseaseRisk,
         alt: "infestation fecal matter"
     },
     {
         header: 'Fire Risk',
-        body: "Rodents, including rats & mice, instinctively gnaw on bjects like electrical wires. This causes expensive repairs and can cause electrical fires. It is estimated that a sebstantial number of fires start with exposed wireing caused by rats and mice",
+        body: "Rodents, including rats & mice, instinctively gnaw on objects like electrical wires. This causes expensive repairs and can cause electrical fires. It is estimated that a substantial number of fires start with exposed wiring caused by rats and mice",
         img: fireRisk,
         alt: "building on fire"
     },
     {
         header: 'AC Systems Damage Risk',
-        body: "Rats & Mice chew into ducts. Once in they cayse your power bills to rise sharply and can destroy your AC unit (avg. cost $6,000 - $8,000). And there is nothing worse than paying to cool your attic.",
+        body: "Rats & Mice chew into ducts. Once in they cause your power bills to rise sharply and can destroy your AC unit (avg. cost $6,000 - $8,000). And there is nothing worse than paying to cool your attic.",
         img: acRisk,
         alt: "ac duct damage"
     },
     {
         header: 'Plumbing/Flooding Risk',
-        body: "Rodenets including rats & mice, cnnot dray water from the eat, they mut seek water sources to survive. They often locate plastic plymbing and ganw into them causeing significant amage and very costly repairs due to water flooding.",
+        body: "Rodents including rats & mice, cannot dray water from the eat, they must seek water sources to survive. They often locate plastic plumbing and gnaw into them causing significant damage and very costly repairs due to water flooding.",
         img: floodRisk,
         alt: "chewed pipe"
     },
     {
         header: 'Social Jugement Risk',
-        body: "People fear rats and mice for good reason. They carry diseases, cause major amge to your property and they are just plain gross. No one wants to be the \"rat house\"",
+        body: "People fear rats and mice for good reason. They carry diseases, cause major damage to your property and they are just plain gross. No one wants to be the \"rat house\"",
         img: judgementRisk,
         alt: "disgusted woman"
     },
@@ -101,7 +101,7 @@ const ratFacts = [
     },
     {
         header: 'Parasites Risk',
-        body: "Rats & Mice bring fleas, ticks and mites (all of which are notorious vectors of disese transmission) with them. As rodents travel or die, they are depsoitiong fleas and ticks everywhereto wait for another live host to infect,. Which, in turn, can cause diseases.",
+        body: "Rats & Mice bring fleas, ticks and mites (all of which are notorious vectors of disease transmission) with them. As rodents travel or die, they are depositing fleas and ticks everywhere to wait for another live host to infect, which, in turn, can cause diseases.",
         img: parasiteRisk,
         alt: 'tick under microscope'
     }
